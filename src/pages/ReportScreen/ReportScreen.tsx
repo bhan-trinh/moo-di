5b9869a90f9c88c3ef898f66dd26bcfc751082be
bhan-trinh <baohanmi@gmail.com>
2025-07-09 16:18:43 +0700

    Add Report Screen

--- src/pages/ReportScreen/ReportScreen.tsx
+++ src/pages/ReportScreen/ReportScreen.tsx
@@ -3,7 +3,11 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from '../../styles/styles';
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { NotesContext } from '../../services/NoteContext';
-import { getDBConnection, getMood, saveNoteItems } from '../../services/NotesDB';
+import {
+  getDBConnection,
+  getMood,
+  saveNoteItems,
+} from '../../services/NotesDB';
 import Icon from '@react-native-vector-icons/lucide';
 import Slider from '@react-native-community/slider';
 import { user } from '../HomeScreen/HomeScreen';
@@ -12,7 +16,7 @@ import { MoodLabel } from '../../models/MoodLabel';
 import { NoteItem } from '../../models/NoteItem';
 
 export const ReportScreen = ({ navigation }) => {
-    /*
+  /*
     const [moods, setMoods] = useState<MoodLabel[]>([])
     useEffect(() => {async () => {
         var db = await getDBConnection()
@@ -20,15 +24,43 @@ export const ReportScreen = ({ navigation }) => {
         setMoods(storedMoods)
     }}, [])
     */
-    var moods = [];
-    const {notes, setNotes} = useContext(NotesContext)
-    moods = notes.map((note: NoteItem) => {
-        return {value: note.mood,
-            label: note.id
-        }
-    })
+  var moodsData = [];
+  const { notes, setNotes } = useContext(NotesContext);
+  var totalMood: number = 0;
+  moodsData = notes.map((note: NoteItem) => {
+    totalMood += note.mood;
+    return { value: note.mood, label: note.id };
+  });
 
-    return (
-    <LineChart data={moods}/>
-)
+  var avgMood = Math.floor(totalMood / moodsData.length);
+  var avgMoodData = moodsData.map(mood => {
+    return {
+      value: avgMood,
+      label: mood.label,
+    };
+  });
+
+  console.log(avgMoodData);
+
+  return (
+    <View>
+      <Text style={styles.welcomeText}>mood report</Text>
+      <View
+        style={{ justifyContent: 'center', alignItems: 'center', padding: 10 }}
+      >
+        <LineChart
+          //   data={avgMoodData}
+          data2={moodsData}
+          //   hideDataPoints1
+          xAxisLabelTextStyle={[styles.text, { fontSize: 15 }]}
+          yAxisTextStyle={[styles.text, { fontSize: 15 }]}
+          color1={'#BBB'}
+          dataPointsColor2={'black'}
+          maxValue={100}
+          mostNegativeValue={1}
+          height={300}
+        />
+      </View>
+    </View>
+  );
 };
